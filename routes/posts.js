503d968a4cd55b897653c3d0ce31cb7d3ec180db
Abbas Abdallah <abbasab13@outlook.com>
2023-05-28 20:56:06 +0300

    updated

--- routes/posts.js
+++ routes/posts.js
@@ -1,11 +1,16 @@
 import express, { Router } from "express"
-import { createPost, deletePost, getAllPosts, likePost, updatePost } from "../controllers/postsControllers.js";
+import { getPost, createPost, deletePost, getAllPosts, search, likePost, commentOnPost, updatePost, deleteComment, getAllPostsPerPage } from "../controllers/postsControllers.js";
 
 import auth from "../middleware/auth.js";
 
 const router = express.Router();
 
-router.get('/', getAllPosts)
+router.get('/', auth, getAllPostsPerPage)
+router.get('/all', auth, getAllPosts)
+router.patch('/comment/:id', auth, commentOnPost)
+router.patch('/comment/delete/:id', auth, deleteComment)
+router.get('/search', search)
+router.get('/:id', getPost)
 router.post('/', auth,createPost)
 router.patch('/:id', auth,updatePost)
 router.delete('/delete/:id', auth,deletePost)
