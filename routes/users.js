ec9882ae7c93a883e1795a7d075a1792f60c0a4c
Abbas Abdallah <abbasab13@outlook.com>
2023-09-13 15:59:24 +0300

    updated

---
+++ routes/users.js
@@ -0,0 +1,18 @@
+import express from 'express'
+import { signin, signup, getUserInfo, updateUserInfo, addFriend, acceptFriend, declineFriend, getFriendDetails, unsendFriendRequest, googleSignIn, unfriend, getUserInfoByEmail } from '../controllers/userControllers.js';
+
+const router = express.Router();
+
+router.post('/signin', signin)
+router.post('/signup', signup)
+router.post('/google-sign-in', googleSignIn)
+router.get('/user/:id', getUserInfo)
+router.post('/user/', getUserInfoByEmail)
+router.patch('/user/update/:id', updateUserInfo)
+router.patch('/user/add/:userId', addFriend)
+router.patch('/user/:userId/unfriend/:loggedUserId', unsendFriendRequest)
+router.patch('/user/accept/:id', acceptFriend)
+router.patch('/user/decline/:id', declineFriend)
+router.patch('/user/unfriend/:id/:loggedUserId', unfriend)
+router.get('/friend/:id', getFriendDetails)
+export default router
