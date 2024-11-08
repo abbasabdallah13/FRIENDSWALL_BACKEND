651949d1ee32a4b7f55a4b7dafcf14ad570a9d3c
Abbas Abdallah <abbasab13@outlook.com>
2024-11-08 15:50:09 +0200

    updated cors

--- index.js
+++ index.js
@@ -15,21 +15,6 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}))
 app.use(cors({ origin: 'https://99c7-94-72-155-139.ngrok-free.app' })); //for development purposes 
 // app.use(cors());
 
-app.use((req, res, next) => {
-    const allowedOrigins = ['https://99c7-94-72-155-139.ngrok-free.app'];
-    const origin = req.headers.origin;
-    if (allowedOrigins.includes(origin)) {
-      res.setHeader('Access-Control-Allow-Origin', origin);
-    }
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  
-    if (req.method === 'OPTIONS') {
-      return res.sendStatus(200); // Always respond OK to OPTIONS preflight
-    }
-    next();
-  });
-
 const PORT = process.env.PORT || 5000;
 
 app.use('/posts',postRoutes)
